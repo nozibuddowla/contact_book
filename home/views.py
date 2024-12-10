09ae1dc18129c23678d0d739f3dedd1145952524
nozibuddowla <nozibuddowla@gmail.com>
2024-12-11 03:59:28 +0600

    show real data from database in our view

--- home/views.py
+++ home/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Contact
 
 # Create your views here.
 def index(request):
+    all_contacts = Contact.objects.all()
+    print(all_contacts)
     context = {
-        'name': 'John Doe', 
+        'contacts': all_contacts, 
     }
     return render(request, 'index.html', context=context)
